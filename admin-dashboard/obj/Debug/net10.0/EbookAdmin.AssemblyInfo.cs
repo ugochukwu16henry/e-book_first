a131502d977722f2a923c7b9a5722921805b9736
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-04-07 13:32:21 +0100

    updated

--- admin-dashboard/obj/Debug/net10.0/EbookAdmin.AssemblyInfo.cs
+++ admin-dashboard/obj/Debug/net10.0/EbookAdmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EbookAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95df2b82e9b95dd564217de45532cfabab574b57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a601b17f2403f271e6939dc687288731097094")]
 [assembly: System.Reflection.AssemblyProductAttribute("EbookAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EbookAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
